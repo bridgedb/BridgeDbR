4d3d0a8ab5d0febebbcd61cb473870c989077f61
Egon Willighagen <egon.willighagen@gmail.com>
2023-09-09 15:58:09 +0200

    Fixed the order to be backwards compatible

--- R/map.R
+++ R/map.R
@@ -1,4 +1,4 @@
-map <- function(mapper, source, identifier, compactIdentifier = NA, target = NULL){
+map <- function(mapper, source, identifier, target = NULL, compactIdentifier = NA) 
   if (!is.na(compactIdentifier)) { 
     source <- gsub(":.*", "", compactIdentifier)
     identifier <- gsub(".*:", "", compactIdentifier)
